0c55387f745f079a17bbb1c35bcce05f1b7fbd3b
agis <agis.septiyanto@gmail.com>
2024-08-14 21:16:25 +0700

    add register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,81 +1,19 @@
-{{--<x-guest-layout>--}}
-{{--    <x-authentication-card>--}}
-{{--        <x-slot name="logo">--}}
-{{--            <x-authentication-card-logo />--}}
-{{--        </x-slot>--}}
-
-{{--        <x-validation-errors class="mb-4" />--}}
-
-{{--        <form method="POST" action="{{ route('register') }}">--}}
-{{--            @csrf--}}
-
-{{--            <div>--}}
-{{--                <x-label for="name" value="{{ __('Name') }}" />--}}
-{{--                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />--}}
-{{--            </div>--}}
-
-{{--            <div class="mt-4">--}}
-{{--                <x-label for="email" value="{{ __('Email') }}" />--}}
-{{--                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />--}}
-{{--            </div>--}}
-
-{{--            <div class="mt-4">--}}
-{{--                <x-label for="password" value="{{ __('Password') }}" />--}}
-{{--                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />--}}
-{{--            </div>--}}
-
-{{--            <div class="mt-4">--}}
-{{--                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />--}}
-{{--                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />--}}
-{{--            </div>--}}
-
-{{--            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())--}}
-{{--                <div class="mt-4">--}}
-{{--                    <x-label for="terms">--}}
-{{--                        <div class="flex items-center">--}}
-{{--                            <x-checkbox name="terms" id="terms" required />--}}
-
-{{--                            <div class="ms-2">--}}
-{{--                                {!! __('I agree to the :terms_of_service and :privacy_policy', [--}}
-{{--                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',--}}
-{{--                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',--}}
-{{--                                ]) !!}--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </x-label>--}}
-{{--                </div>--}}
-{{--            @endif--}}
-
-{{--            <div class="flex items-center justify-end mt-4">--}}
-{{--                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">--}}
-{{--                    {{ __('Already registered?') }}--}}
-{{--                </a>--}}
-
-{{--                <x-button class="ms-4">--}}
-{{--                    {{ __('Register') }}--}}
-{{--                </x-button>--}}
-{{--            </div>--}}
-{{--        </form>--}}
-{{--    </x-authentication-card>--}}
-{{--</x-guest-layout>--}}
-
-
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 
     <title>Pages / Register - NiceAdmin Bootstrap Template</title>
-    <meta content="" name="description" />
-    <meta content="" name="keywords" />
+    <meta content="" name="description"/>
+    <meta content="" name="keywords"/>
 
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon" />
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+    <link href="assets/img/favicon.png" rel="icon"/>
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"/>
 
     <!-- Google Fonts -->
-    <link href="https://fonts.gstatic.com" rel="preconnect" />
+    <link href="https://fonts.gstatic.com" rel="preconnect"/>
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
         rel="stylesheet"
@@ -90,14 +28,14 @@
         href="_dashboard/assets/vendor/bootstrap-icons/bootstrap-icons.css"
         rel="stylesheet"
     />
-    <link href="_dashboard/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
-    <link href="_dashboard/assets/vendor/quill/quill.snow.css" rel="stylesheet" />
-    <link href="_dashboard/assets/vendor/quill/quill.bubble.css" rel="stylesheet" />
-    <link href="_dashboard/assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
-    <link href="_dashboard/assets/vendor/simple-datatables/style.css" rel="stylesheet" />
+    <link href="_dashboard/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"/>
+    <link href="_dashboard/assets/vendor/quill/quill.snow.css" rel="stylesheet"/>
+    <link href="_dashboard/assets/vendor/quill/quill.bubble.css" rel="stylesheet"/>
+    <link href="_dashboard/assets/vendor/remixicon/remixicon.css" rel="stylesheet"/>
+    <link href="_dashboard/assets/vendor/simple-datatables/style.css" rel="stylesheet"/>
 
     <!-- Template Main CSS File -->
-    <link href="_dashboard/assets/css/style.css" rel="stylesheet" />
+    <link href="_dashboard/assets/css/style.css" rel="stylesheet"/>
 
     <!-- =======================================================
   * Template Name: NiceAdmin
@@ -124,7 +62,7 @@
                                 href="index.html"
                                 class="logo d-flex align-items-center w-auto"
                             >
-                                <img src="_dashboard/assets/img/logo.png" alt="" />
+                                <img src="_dashboard/assets/img/logo.png" alt=""/>
                                 <span class="d-none d-lg-block">Suratmi Homecare</span>
                             </a>
                         </div>
@@ -138,7 +76,9 @@
                                     </h5>
                                 </div>
 
-                                <form class="row g-3 needs-validation" novalidate>
+                                <form class="row g-3 needs-validation" novalidate action="{{ route('register') }}"
+                                      method="POST">
+                                    @csrf
                                     <div class="col-12">
                                         <label for="yourName" class="form-label">Name</label>
                                         <input
@@ -174,6 +114,22 @@
                                         <input
                                             type="password"
                                             name="password"
+                                            class="form-control"
+                                            id="yourPassword"
+                                            required
+                                        />
+                                        <div class="invalid-feedback">
+                                            Please enter your password!
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <label for="yourPassword" class="form-label"
+                                        >Password Confirmation</label
+                                        >
+                                        <input
+                                            type="password"
+                                            name="password_confirmation"
                                             class="form-control"
                                             id="yourPassword"
                                             required
