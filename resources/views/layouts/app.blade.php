69d8e1df3ab5e4f0a0843d5456ff9080466c8456
difawitsqard <difawitsqard@gmail.com>
2024-08-15 09:44:05 +0700

    set text modal color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -287,7 +287,9 @@
     </main>
     <!-- End #main -->
 
-    @stack('modals')
+    <div style="color: #212529;">
+        @stack('modals')
+    </div>
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
