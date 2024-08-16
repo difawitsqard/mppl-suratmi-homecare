0aba9bae50cb8c9629ce31b3bb45c144a89f15ec
agis <agis.septiyanto@gmail.com>
2024-08-17 06:27:22 +0700

    enable to add prices

--- resources/views/dashboard/service-management/index.blade.php
+++ resources/views/dashboard/service-management/index.blade.php
@@ -49,18 +49,22 @@
         <div class="modal-dialog">
             <div class="modal-content text-black">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="addServiceModalLabel">Add Service</h5>
+                    <h5 class="modal-title" id="addServiceModalLabel">Tambah Layanan</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <form action="{{ route('dashboard.service-management.store') }}" method="POST">
                         @csrf
                         <div class="mb-3">
-                            <label for="name" class="form-label ">Name</label>
+                            <label for="name" class="form-label">Nama</label>
                             <input type="text" class="form-control" id="name" name="name" required>
                         </div>
                         <div class="mb-3">
-                            <label for="description" class="form-label">Description</label>
+                            <label for="price" class="form-label">Price</label>
+                            <input type="number" class="form-control" id="price" name="price" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Deskripsi</label>
                             <textarea class="form-control" id="description" name="description" rows="3"></textarea>
                         </div>
                         <button type="submit" class="btn btn-primary">Save</button>
