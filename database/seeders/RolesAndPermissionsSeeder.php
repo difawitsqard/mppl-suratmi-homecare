5945e50c536797c25c9aeb4a3b15e2c292d62fa5
difawitsqard <difawitsqard@gmail.com>
2024-08-17 10:38:33 +0700

    remove superadmin role

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -15,7 +15,6 @@ class RolesAndPermissionsSeeder extends Seeder
     {
         // Membuat roles
         $roles = [
-            'superadmin',
             'admin',
             'therapist',
             'customer',
